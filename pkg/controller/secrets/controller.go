62658748b880e064bf1d480991aa84c48967354a
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2019-03-06 08:36:42 -0500

    Testing secrets lister.

---
+++ pkg/controller/secrets/controller.go
@@ -0,0 +1,134 @@
+package secrets
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog"
+	"k8s.io/client-go/tools/cache"
+
+	corelisters "k8s.io/client-go/listers/core/v1"
+
+	controllerpkg "github.com/jetstack/cert-manager/pkg/controller"
+	
+	cmlisters "github.com/jetstack/cert-manager/pkg/client/listers/certmanager/v1alpha1"
+	"github.com/jetstack/cert-manager/pkg/scheduler"
+)
+
+type Controller struct {
+	*controllerpkg.context
+
+	syncHandler func(ctx context.Context, key string) error
+
+	secretLister 		corelisters.SecretLister
+	certificateLister 	cmlisters.CertificateLister
+
+	queue              workqueue.RateLimitingInterface
+	scheduledWorkQueue scheduler.ScheduledWorkQueue
+	workerWg           sync.WaitGroup
+	syncedFuncs        []cache.InformerSynced
+}
+
+func New(ctx *controllerpkg.Context) *Controller {
+	ctrl := &Controller{Context: ctx}
+	ctrl.syncHandler = ctrl.processNextWorkItem
+	ctrl.queue = workqueue.NewNamedRateLimitingQueue(controllerpkg.DefaultItemBasedRateLimiter(), "certificates")
+
+	// Create a scheduled work queue that calls the ctrl.queue.Add method for
+	// each object in the queue. This is used to schedule re-checks of
+	// Certificate resources when they get near to expiry
+	ctrl.scheduledWorkQueue = scheduler.NewScheduledWorkQueue(ctrl.queue.AddRateLimited)
+
+	certificateInformer := ctrl.SharedInformerFactory.Certmanager().V1alpha1().Certificates()
+	certificateInformer.Informer().AddEventHandler(&controllerpkg.QueuingEventHandler{Queue: ctrl.queue})
+	ctrl.certificateLister = certificateInformer.Lister()
+	ctrl.syncedFuncs = append(ctrl.syncedFuncs, certificateInformer.Informer().HasSynced)
+
+	secretInformer := ctrl.KubeSharedInformerFactory.Core().V1().Secrets()
+	secretsInformer.Informer().AddEventHandler(&controllerpkg.BlockingEventHandler{WorkFunc: ctrl.handleSecretResource})
+	ctrl.secretLister = secretsInformer.Lister()
+	ctrl.syncedFuncs = append(ctrl.syncedFuncs, secretsInformer.Informer().HasSynced)
+
+	return ctrl
+}
+
+func (c *Controller) Run(workers int, stopCh <-chan struct{}) error {
+	klog.Info("Running secrets controller")
+	if !cache.WaitForCacheSync(stopch, c.syncedFuncts...) {
+		return fmt.Errorf("error waiting for informer caches to sync")
+	}
+
+	for i:= 0; i < workers; i++ {
+		c.workerWg.Add(1)
+		go wait.Until(func() { c.worker(stopCh) }, time.Second, stopCh)
+	}
+	<-stopCh
+	c.queue.ShutDown()
+	c.workerWg.Wait()
+	return nil
+}
+
+func (c *Controller) worker(stopCh <-chan struct{}) {
+	defer c.workerWg.Done()
+	for {
+		obj, shutdown := c.queue.Get()
+		if shutdown {
+			break
+		}
+		var key string
+		func() {
+			defer c.queue.Done(obj)
+			var ok bool
+			if key, ok = obj.(string); !ok {
+				return
+			}
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			ctx = util.ContextWithStopCh(ctx, stopCh)
+			if err := c.syncHandler(ctx, key); err != nil {
+				klog.Errorf("%s controller: Re-queuing item %q due to error processing: %s", ControllerName, key, err.Error())
+				c.queue.AddRateLimited(obj)
+				return
+			}
+			klog.Infof("%s controller: Finished processing work item %q", ControllerName, key)
+			c.queue.Forget(obj)
+		}()
+	}
+	klog.V(4).Infof("Exiting %q worker loop", ControllerName)
+}
+
+func (c *Controller) processNextWorkItem(ctx context.Context, key string) error {
+	namespace, name, err := cache.SplitMetaNamespaceKey(key)
+	klog.Info("Next work item: \nNamespace: %s, Name: %s", namespace, name)
+	if err != nil {
+		runtime.HandleError(fmt.Errorf("invalid resource key: %s", key))
+		return nil
+	}
+	secret, err := c.secretLister.Secret(namespace).Get(name)
+	if err != nil {
+		if k8sErrors.IsNotFound(err) {
+			c.scheduledWorkQueue.Forget(key)
+			runtime.HandleError(fmt.Errorf("secret '%s' in work queue no longer exists", key))
+			return nil
+		}
+
+		return err
+	}
+
+	return c.Sync(ctx, secret)
+}
+
+var keyFunc = controllerpkg.KeyFunc
+
+const (
+	ControllerName = "certificates"
+)
+
+func init() {
+	controllerpkg.Register(ControllerName, func(ctx *controllerpkg.Context) controllerpkg.Interface {
+		return New(ctx).Run
+	})
+}
---
+++ pkg/controller/secrets/sync.go
@@ -0,0 +1,38 @@
+package secrets
+
+import (
+	"os"
+	"context"
+	"k8s.io/klog"
+	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha1"
+	"github.com/jetstack/cert-manager/pkg/util/kube"
+)
+
+func (c *Controller) Sync(ctx context.Context, secret *corev1.Secret) {
+	klog.Infof("%v", secret)
+
+	// Figure out if certificate has associated cert manager certificate
+	crtName := secret.Labels[v1alpha1.CertificateNameKey]
+	crt, _ := c.certificateLister().Certificate(namespace).Get(crtName)
+	if crtName == nil || crt == nil {
+		klog.Info("Associated cert manager cert does not exist with this secret")
+		// Decode the certificate in the secret
+		//x509crt := kube.SecretTLSCertName(c.secretLister, namespace, secret.ObjectMeta.Name, )
+		// Create the cert manager certificate
+		/*crt = &v1alpha1.Certificate{
+			TypeMeta: metav1.TypeMeta
+			ObjectMeta: metav1.ObjectMeta{
+				Name: 
+				Namespace: namespace,
+			},
+			Spec: v1alpha1.CertificateSpec{
+				SecretName: secret.ObjectMeta.Name
+			},
+			Status: v1alpha1.CertificateStatus{
+
+			}
+		}*/
+	}
+
+}
