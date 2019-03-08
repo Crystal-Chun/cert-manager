89b638561e2fe35dca776264edec4cbeb9ec4815
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2019-03-08 11:50:59 -0500

    Removed unused imports.

--- pkg/controller/secrets/sync.go
+++ pkg/controller/secrets/sync.go
@@ -5,9 +5,9 @@ import (
 	"k8s.io/klog"
 	"time"
 	"strings"
-	"crypto/x509"
+	
 	"crypto/rsa"
-	"encoding/pem"
+	
 	"fmt"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1 "k8s.io/api/core/v1"
